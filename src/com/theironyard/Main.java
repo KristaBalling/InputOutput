3eebb15b8de2bbdccfbcd81078fcc02cb13b0865
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-08 15:03:36 -0500

    added _big txt files and changed starting loc

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -54,7 +54,8 @@ public class Main {
         vocabulary.put("EAST", "E");
 
 
-        int loc = 1;
+        int loc = 64;
+//        int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
             if(loc == 0) {
