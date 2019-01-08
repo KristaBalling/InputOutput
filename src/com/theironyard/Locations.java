4e09c3e1d3d3a2edb03f09fcc8094dd8729d1399
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-08 13:18:47 -0500

    writes output to txt file

--- src/com/theironyard/Locations.java
+++ src/com/theironyard/Locations.java
@@ -1,12 +1,38 @@
 package com.theironyard;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Locations implements Map<Integer, Location> {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Map<Integer, Location> locations = new HashMap<>();
+
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("locations.txt");
+            for (Location location : locations.values()) {
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+            }
+            locFile.close();
+        }catch(IOException e) {
+            System.out.println("In catch block");
+            e.printStackTrace();
+        } finally {
+            System.out.println("In finally block");
+            try{
+                if(locFile != null) {
+                    System.out.println("Attempting to close locfile");
+                    locFile.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
