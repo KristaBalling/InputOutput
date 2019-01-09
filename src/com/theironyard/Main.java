fc64da316a18de25d8ae889d56434fa4c9d9cc64
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-09 16:00:02 -0500

    exercise complete. with deprecations

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,6 @@
 package com.theironyard;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -7,7 +8,7 @@ import java.util.Scanner;
 public class Main {
     private static Locations locations = new Locations();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Change the program to allow players to type full words, or phrases, then move to the
         // correct location based upon their input.
         // The player should be able to type commands such as "Go West", "run South", or just "East"
@@ -18,34 +19,6 @@ public class Main {
 
 	    Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
-
-        tempExit = new HashMap<String, Integer>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
-
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
@@ -54,15 +27,14 @@ public class Main {
         vocabulary.put("EAST", "E");
 
 
-        int loc = 64;
-//        int loc = 1;
+        Location currentLocation = locations.getLocation(1);
         while(true) {
-            System.out.println(locations.get(loc).getDescription());
-            if(loc == 0) {
+            System.out.println(currentLocation.getDescription());
+            if(currentLocation.getLocationID() == 0) {
                 break;
             }
 
-            Map<String, Integer> exits = locations.get(loc).getExits();
+            Map<String, Integer> exits = currentLocation.getExits();
             System.out.print("Available exits are ");
             for(String exit: exits.keySet()) {
                 System.out.print(exit + ", ");
@@ -81,12 +53,14 @@ public class Main {
             }
 
             if(exits.containsKey(direction)) {
-                loc = exits.get(direction);
+                currentLocation = locations.getLocation(currentLocation.getExits().get(direction));
 
             } else {
                 System.out.println("You cannot go in that direction");
             }
         }
+
+        locations.close();
 
     }
 }
